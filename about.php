ed3ff8f27ea57100a3922ad07117fcc24fb39b9f
joshuuu000 <joshuadelatorrecute@gmail.com>
2025-10-17 20:15:08 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,51 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                                    St. Elizabeth Global Skills Institute Inc. commits itself to being a pioneer
+                                    in providing quality education to its students and being a supportive
+                                    pillar of the community. We envision producing a globally exceptional
+                                    workforce that espouses the moral values of being a good Christian.
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                    St. Elizabeth Global Skills Institute Inc. aims to form and develop
+                                    students with necessary knowledge, skills and competencies to become
+                                    competitive members of the global workforce. Our Methodology is
+                                    aligned with a Christian-based approach to give students the proper
+                                    moral acumen and embody the values of our patron,
+                                    Saint Elizabeth Ann Seton.
+
+                                     
+
+                                   <h1> CORE VALUES</h1>
+                                   <ul>
+                                    <li>Professionalism- Always conduct yourself in the highest regard. Be excellent in all that you do.</li>
+                                    <li>Adaptability- Be prepared to be challenged. Overcome and adapt.</li>
+                                    <li>Virtuosity- Look to God and our patron for guidance. In the Lord we trust.</li>
+                                    <li>Integrity-Remember your principles. Become an inspiration and example.</li>
+                                    <li>Compassion-Aim to uplift. Aim to assist, provide succor.</li>
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
---
+++ home.php
@@ -0,0 +1,84 @@
+ <!-- Map Column -->
+ <div class="row">
+            <div class="col-md-4">
+                <img width="100%" src="img/Set_logo.jpg">
+            </div>
+            <!-- Contact Details Column -->
+            <div class="col-md-8">
+                <h3>Contact Details</h3>
+                <p>
+                    St. Elizabeth Global Skills Institute Inc. 
+                <br>Km 131, Maharlika Highway Esguerra Dist. Talavera, Nueva Ecija, Philippines<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: 0967-815-0446</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">Email</abbr>: <a href="mailto:name@example.com">St.Elizabethtalavera@gmail.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 8:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+</div>
+    <!-- /.row -->
+          
+                     <br/>
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                       <!--  <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div> -->
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                                    St. Elizabeth Global Skills Institute Inc. commits itself to being a pioneer
+                                    in providing quality education to its students and being a supportive
+                                    pillar of the community. We envision producing a globally exceptional
+                                    workforce that espouses the moral values of being a good Christian.
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                    St. Elizabeth Global Skills Institute Inc. aims to form and develop
+                                    students with necessary knowledge, skills and competencies to become
+                                    competitive members of the global workforce. Our Methodology is
+                                    aligned with a Christian-based approach to give students the proper
+                                    moral acumen and embody the values of our patron,
+                                    Saint Elizabeth Ann Seton.
+                                     
+
+                                   <h1> GOALS</h1>
+                                   <ul>
+                                    <li>Professionalism- Always conduct yourself in the highest regard. Be excellent in all that you do.</li>
+                                    <li>Adaptability- Be prepared to be challenged. Overcome and adapt.</li>
+                                    <li>Virtuosity- Look to God and our patron for guidance. In the Lord we trust.</li>
+                                    <li>Integrity-Remember your principles. Become an inspiration and example.</li>
+                                    <li>Compassion-Aim to uplift. Aim to assist, provide succor.</li>
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
